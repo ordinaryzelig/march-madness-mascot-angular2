9738e13a567fd4b144943407bc6453a3dd379d7d
Jared Ning <jared@redningja.com>
2016-10-06 13:35:00 -0500

    Remove unused MascotService#selectedMascots for now.

--- src/app/mascot.service.ts
+++ src/app/mascot.service.ts
@@ -2,8 +2,6 @@ import { MASCOT_DATA } from './mascot-data.ts';
 import { Mascot }      from './mascot.ts';
 
 export class MascotService {
-  selectedMascots = [];
-
   mascotsForYear(year): Mascot[] {
     let mascots = [];
     for (let atts of MASCOT_DATA[year]) {
